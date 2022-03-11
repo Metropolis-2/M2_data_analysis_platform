2308d752fbf939a29a1eb4925bbe2647b2eacb89
nipatrin <nipatrin@gmail.com>
2022-03-11 18:45:16 +0200

    Update dataframes' structure

--- platform_code/MainApp.py
+++ platform_code/MainApp.py
@@ -27,7 +27,7 @@ class MainClass():
         self.scenario_name = None
 
         #self.readLogFiles()
-        self.dataframe_creator = DataframeCreator.DataframeCreator(self.scenario_name, self.spark)
+        self.dataframe_creator = DataframeCreator.DataframeCreator( self.spark) # the dataframes are created in  the init function now
 
         self.fp_intention_dataframe = None #TODO: It was necessary a fpintention dataframe to manage the columns of the file
         self.loslog_dataframe = None
@@ -35,14 +35,14 @@ class MainClass():
         self.geolog_dataframe = None
         self.flst_log_dataframe = None
         self.reglog_obj_dataframe = None
-        self.createDataframes()
+        #self.createDataframes()
 
        # self.AEQ_metrics = AEQ_metrics.AEQ_metrics(self.fp_intention_dataframe, self.flst_log_dataframe)
-        self.CAP_metrics = CAP_metrics.CAP_metrics(self.fp_intention_dataframe, self.flst_log_dataframe, self.loslog_dataframe)
-        self.EFF_metrics = EFF_metrics.EFF_metrics(self.fp_intention_dataframe, self.flst_log_dataframe)
-        self.ENV_metrics = ENV_metrics.ENV_metrics(self.flst_log_dataframe)
-        self.SAF_metrics = SAF_metrics.SAF_metrics(self.loslog_dataframe, self.conflog_dataframe, self.geolog_dataframe)
-        self.PRI_metrics = PRI_metrics.PRI_metrics(self.fp_intention_dataframe, self.flst_log_dataframe)
+        #self.CAP_metrics = CAP_metrics.CAP_metrics(self.fp_intention_dataframe, self.flst_log_dataframe, self.loslog_dataframe)
+        #self.EFF_metrics = EFF_metrics.EFF_metrics(self.fp_intention_dataframe, self.flst_log_dataframe)
+        #self.ENV_metrics = ENV_metrics.ENV_metrics(self.flst_log_dataframe)
+        #self.SAF_metrics = SAF_metrics.SAF_metrics(self.loslog_dataframe, self.conflog_dataframe, self.geolog_dataframe)
+        #self.PRI_metrics = PRI_metrics.PRI_metrics(self.fp_intention_dataframe, self.flst_log_dataframe)
         
 # =============================================================================
 #     def readLogFiles(self):
@@ -57,17 +57,15 @@ class MainClass():
         
 
     def createDataframes(self):
-        self.fp_intention_dataframe = self.dataframe_creator.create_fp_intention_dataframe(
-            "example_logs/Flight_intention_very_low_40_8.csv")
-        self.loslog_dataframe = self.dataframe_creator.create_loslog_dataframe()
-        self.conflog_dataframe = self.dataframe_creator.create_conflog_dataframe()
+        #self.fp_intention_dataframe = self.dataframe_creator.create_fp_intention_dataframe(
+        #    "example_logs/Flight_intention_very_low_40_8.csv")
+        self.loslog_dataframe = self.dataframe_creator.create_loslog_dataframe() 
+        self.conflog_dataframe = self.dataframe_creator.create_conflog_dataframe() 
         self.geolog_dataframe = self.dataframe_creator.create_geolog_dataframe()
-        self.flst_log_dataframe = self.dataframe_creator.create_flstlog_dataframe()
+        self.flst_log_dataframe = self.dataframe_creator.create_flstlog_dataframe() 
         self.reglog_obj_dataframe = self.dataframe_creator.create_reglog_dataframe()
-
-        
-        time_log_dataframe=self.dataframe_creator.create_time_object_dataframe()
-        metrics_dataframe=self.dataframe_creator.create_metrics_dataframe()
+        time_log_dataframe=self.dataframe_creator.create_time_object_dataframe() 
+        metrics_dataframe=self.dataframe_creator.create_metrics_dataframe() 
 
     def main(self):
 
