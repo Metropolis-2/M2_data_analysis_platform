d31400bda313281366eed24daf48615007e8473e
jpedrero <juan.pedrero.gonzalez@aerospaceanddefense.eu>
2022-03-23 17:26:57 +0100

    [develop_everis] PRI_metrics notebook updated and PRI5 resolved. Added some baseline functions (BASELINE_2D_DISTANCE, BASELINE_FLIGHT_TIME)

--- platform_code/PRI_metrics.py
+++ platform_code/PRI_metrics.py
@@ -1,211 +1,63 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Feb 24 10:49:23 2022
+from parse.parser_constants import FLST_LOG_PREFIX
+from schemas.tables_attributes import FLIGHT_TIME, SCENARIO_NAME, PRIORITY, PRI1, BASELINE_3D_DISTANCE, PRI2, \
+    DISTANCE_3D, ACID, SPAWN_TIME, BASELINE_DEPARTURE_TIME
+import pyspark.sql.functions as F
+from pyspark.sql.functions import lit, col
 
-@author: jpedrero
-"""
-import utils
-from platform_code.ctes import Ctes
+def compute_PRI1_metric(spark, input_df, output_df):
+    '''
+    PRI-1: Weighted mission duration
+    (Total duration of missions weighted in function of priority level)
+    '''
+    df = output_df["OUTPUT"]
+    query_rows = input_df[FLST_LOG_PREFIX].groupby(SCENARIO_NAME, PRIORITY).agg((F.sum(FLIGHT_TIME) * col(PRIORITY)).alias(PRI1)).groupby(SCENARIO_NAME).agg(F.sum(PRI1))
+    df = df.join(query_rows, on=[SCENARIO_NAME], how='outer')
+    return df
 
+def compute_PRI2_metric(spark, input_df, output_df):
+    '''
+    PRI-2: Weighted mission track length
+    (Total distance travelled weighted in function of priority level)
+    '''
+    df = output_df["OUTPUT"]
+    query_rows = input_df[FLST_LOG_PREFIX].groupby(SCENARIO_NAME, PRIORITY).agg((F.sum(DISTANCE_3D) * col(PRIORITY)).alias(PRI2)).groupby(SCENARIO_NAME).agg(F.sum(PRI2))
+    df = df.join(query_rows, on=[SCENARIO_NAME], how='outer')
+    return df
 
-class PRI_metrics():
-    
-    def __init__(self, flst_log_dataframe):
-        self.ctes = Ctes()
-        self.utils = utils.Utils()
-        self.flst_log_dataframe = flst_log_dataframe
+def compute_PRI3_metric(spark, input_df, output_df):
+    '''
+    PRI-3: Average mission duration per priority level
+    (The average mission duration for each priority level per aircraft)
+    '''
+    df = output_df["OUTPUT"]
+    df1 = input_df[FLST_LOG_PREFIX].groupby(SCENARIO_NAME, PRIORITY).agg(F.sum(FLIGHT_TIME).alias("Tprio"))
+    df2 = input_df[FLST_LOG_PREFIX].groupby(SCENARIO_NAME, PRIORITY).count().withColumnRenamed("count", "Nprio")
+    df3 = df1.join(df2, on=[SCENARIO_NAME, PRIORITY], how='outer')
+    df3 = df3.withColumn("PRIO3", df3.Tprio / df3.Nprio)
+    #TODO: drop Tprio and Nprio in df3
+    df = df.join(df3, on=[SCENARIO_NAME], how='outer')
+    return df
 
-        #Extract list ACIDs of each priority type
-        self.prio1_list = [str(row["ACID"]) for row in
-                     self.flst_log_dataframe.select("ACID").filter("Priority==1").collect()]
-        self.num_prio1 = len(self.prio1_list)
-        self.prio2_list = [str(row["ACID"]) for row in
-                     self.flst_log_dataframe.select("ACID").filter("Priority==2").collect()]
-        self.num_prio2 = len(self.prio2_list)
-        self.prio3_list = [str(row["ACID"]) for row in
-                     self.flst_log_dataframe.select("ACID").filter("Priority==3").collect()]
-        self.num_prio3 = len(self.prio3_list)
-        self.prio4_list = [str(row["ACID"]) for row in
-                     self.flst_log_dataframe.select("ACID").filter("Priority==4").collect()]
-        self.num_prio4 = len(self.prio4_list)
-        return
-        
-    def evaluate_PRI_metric(self, metric_id):
-        if(metric_id == 1):
-            return self.compute_PRI1_metric()
-        elif(metric_id == 2):
-            return self.compute_PRI2_metric()
-        elif(metric_id == 3):
-            return self.compute_PRI3_metric()
-        elif(metric_id == 4):
-            return self.compute_PRI4_metric()
-        elif(metric_id == 5):
-            return self.compute_PRI5_metric()
-        else:
-            print("No valid metric")
-    
-    def compute_PRI1_metric(self):
-        '''
-        PRI-1: Weighted mission duration
-        (Total duration of missions weighted in function of priority level)
-        '''
+def compute_PRI4_metric(spark, input_df, output_df):
+    '''
+    PRI-4: Average mission track length per priority level
+    (The average distance travelled for each priority level per aircraft)
+    '''
+    df = output_df["OUTPUT"]
+    df1 = input_df[FLST_LOG_PREFIX].groupby(SCENARIO_NAME, PRIORITY).agg(F.sum(DISTANCE_3D).alias("Dprio"))
+    df2 = input_df[FLST_LOG_PREFIX].groupby(SCENARIO_NAME, PRIORITY).count().withColumnRenamed("count", "Nprio")
+    df3 = df1.join(df2, on=[SCENARIO_NAME, PRIORITY], how='outer')
+    df3 = df3.withColumn("PRIO4", df3.Dprio / df3.Nprio)
+    #TODO: drop Dprio and Nprio in df3
+    df = df.join(df3, on=[SCENARIO_NAME], how='outer')
+    return df
 
-        #Get the flight_time of each vehicle ACID and sum by prio type
-        fptimes_prio1 = sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio1_list)).collect()])
-        fptimes_prio2= sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio2_list)).collect()])
-        fptimes_prio3 = sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio3_list)).collect()])
-        fptimes_prio4 = sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio4_list)).collect()])
-
-        result = (self.ctes.WEIGHT_PRIO1*fptimes_prio1) + (self.ctes.WEIGHT_PRIO2*fptimes_prio2) + (self.ctes.WEIGHT_PRIO3*fptimes_prio3) + (self.WEIGHT_PRIO4*fptimes_prio4)
-        return result
-    
-    def compute_PRI2_metric(self):
-        '''
-        PRI-2: Weighted mission track length
-        (Total distance travelled weighted in function of priority level)
-        '''
-
-        #TODO: is based on 2D_dist, 3D_dist, Baseline_2d_path_length or Baseline_3d_path_length?
-        #Get the distance missions of each ACID and sum by prio type
-        fpdist_prio1 = sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio1_list)).collect()])
-        fpdist_prio2= sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio2_list)).collect()])
-        fpdist_prio3 = sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio3_list)).collect()])
-        fpdist_prio4 = sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio4_list)).collect()])
-
-        result = (self.ctes.WEIGHT_PRIO1*fpdist_prio1) + (self.ctes.WEIGHT_PRIO2*fpdist_prio2) + (self.ctes.WEIGHT_PRIO3*fpdist_prio3) + (self.WEIGHT_PRIO4*fpdist_prio4)
-        return result
-    
-    def compute_PRI3_metric(self):
-        '''
-        PRI-3: Average mission duration per priority level
-        (The average mission duration for each priority level per aircraft)
-        '''
-        #Get the flight_time of each ACID and sum by type
-        fptimes_prio1 = sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe["ACID"]).isin(self.prio1_list)).collect()])
-        fptimes_prio2= sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe["ACID"]).isin(self.prio2_list)).collect()])
-        fptimes_prio3 = sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe["ACID"]).isin(self.prio3_list)).collect()])
-        fptimes_prio4 = sum([float(row["FLIGHT_time"]) for row in self.flst_log_dataframe.select("FLIGHT_time").filter(
-            (self.flst_log_dataframe["ACID"]).isin(self.prio4_list)).collect()])
-
-        #Calculate the flight_time average value with the number of each prio type vehicles
-        if(self.num_prio1 >0):
-            result_prio1 = fptimes_prio1/self.num_prio1
-        else:
-            result_prio1 = 0
-
-        if(self.num_prio2>0):
-            result_prio2 = fptimes_prio2/self.num_prio2
-        else:
-            result_prio2 = 0
-
-        if(self.num_prio3 >0):
-            result_prio3 = fptimes_prio3/self.num_prio3
-        else:
-            result_prio3 = 0
-
-        if(self.num_prio4 >0):
-            result_prio4 = fptimes_prio4/self.num_prio4
-        else:
-            result_prio4 = 0
-
-        result = (result_prio1, result_prio2, result_prio3, result_prio4) #TODO: return result, 4 params?
-        return result
-    
-    def compute_PRI4_metric(self):
-        '''
-        PRI-4: Average mission track length per priority level
-        (The average distance travelled for each priority level per aircraft)
-        '''
-        # TODO: is based on 2D_dist, 3D_dist, Baseline_2d_path_length or Baseline_3d_path_length?
-        #Get the distance missions of each ACID and sum by type
-        fpdist_prio1 = sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio1_list)).collect()])
-        fpdist_prio2= sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio2_list)).collect()])
-        fpdist_prio3 = sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio3_list)).collect()])
-        fpdist_prio4 = sum([float(row["3D_dist"]) for row in self.flst_log_dataframe.select("3D_dist").filter(
-            (self.flst_log_dataframe.ACID).isin(self.prio4_list)).collect()])
-
-        # Calculate the distance missions average value with the number of each prio type vehicles
-        if(self.num_prio1 >0):
-            result_prio1 = fpdist_prio1/self.num_prio1
-        else:
-            result_prio1 = 0
-
-        if(self.num_prio2>0):
-            result_prio2 = fpdist_prio2/self.num_prio2
-        else:
-            result_prio2 = 0
-
-        if(self.num_prio3 >0):
-            result_prio3 = fpdist_prio3/self.num_prio3
-        else:
-            result_prio3 = 0
-
-        if(self.num_prio4 >0):
-            result_prio4 = fpdist_prio4/self.num_prio4
-        else:
-            result_prio4 = 0
-
-        result = (result_prio1, result_prio2, result_prio3, result_prio4) #TODO: return result, 4 params?
-        return result
-
-    def compute_PRI5_metric(self):
-        '''
-        PRI-5: Total delay per priority level
-        (The total delay experienced by aircraft in a certain priority category relative to ideal conditions)
-        '''
-        #TODO: With what we know from the fp_intention file and the FLSTLOG log file,
-        # we can only calculate the delay that each flight plan mission had for each vehicle and make the sum
-
-        #Extract departure_time of fplanintention of each ACID
-        departure_time_dict = dict([(str(row.ACID),[self.utils.get_sec(str(row.Baseline_deparure_time))]) for row in self.flst_log_dataframe.select("ACID", "Baseline_deparure_time").collect()])
-        #Extract spawn_time of FLSTLOG of each ACID
-        spawn_time_dict = dict([(str(row["ACID"]), [int(row["SPAWN_time"])]) for row in self.flst_log_dataframe.select("ACID","SPAWN_time").collect()])
-        print("len(departure_time_dict): {} and values: {}".format(len(departure_time_dict),departure_time_dict))
-        print("len(spawn_time_dict): {} and values: {}".format(len(spawn_time_dict),spawn_time_dict))
-
-        # Merge dictionaries and add values of common keys in a list. [key1] = [departure_time1, spawn_time1]
-        merged_dict = self.utils.mergeDict(departure_time_dict, spawn_time_dict)
-        merged_dict = dict((k, v) for k, v in merged_dict.items() if len(v)>1) #keep only the mission approved (spawned flights has value list >1)
-        print('merged_dict: {}'.format(merged_dict))
-
-        delays_dict_prio1 = {} #values in seconds
-        delays_dict_prio2 = {}  # values in seconds
-        delays_dict_prio3 = {}  # values in seconds
-        delays_dict_prio4 = {}  # values in seconds
-        total_delay_prio1 = 0 #in seconds
-        total_delay_prio2 = 0  # in seconds
-        total_delay_prio3 = 0  # in seconds
-        total_delay_prio4 = 0  # in seconds
-        for k, v in merged_dict.items():
-            delay = abs(v[0] - v[1]) #Calculate delay between departure_time and spawn_time
-            if(k in self.prio1_list): #acids with prio1
-                delays_dict_prio1[k] = delay
-                total_delay_prio1 += delay
-            elif(k in self.prio1_list): #acids with prio2
-                delays_dict_prio2[k] = delay
-                total_delay_prio2 += delay
-            elif(k in self.prio1_list): #acids with prio3
-                delays_dict_prio3[k] = delay
-                total_delay_prio3 += delay
-            else: #acids with prio4
-                delays_dict_prio4[k] = delay
-                total_delay_prio4 += delay
-
-        final_total_delay = (total_delay_prio1, total_delay_prio2, total_delay_prio3, total_delay_prio4)
-        print("final_total_delay: {}".format(final_total_delay))
-        result = final_total_delay #TODO: return result, 4 params?
-        return result
+def compute_PRI5_metric(spark, input_df, output_df):
+    '''
+    PRI-5: Total delay per priority level
+    (The total delay experienced by aircraft in a certain priority category relative to ideal conditions)
+    '''
+    df = output_df["OUTPUT"]
+    df1 = input_df[FLST_LOG_PREFIX].withColumn("out", input_df[FLST_LOG_PREFIX][SPAWN_TIME] - input_df[FLST_LOG_PREFIX][BASELINE_DEPARTURE_TIME]).groupby(SCENARIO_NAME, ACID).agg(F.sum("PRI5"))
+    df = df.join(df1, on=[SCENARIO_NAME, ACID], how='outer')
+    return df
--- platform_code/SAF_metrics.py
+++ platform_code/SAF_metrics.py
@@ -1,6 +1,5 @@
-
-from parse.parser_constants import CONF_LOG_PREFIX, LOS_LOG_PREFIX
-from schemas.tables_attributes import SCENARIO_NAME, SAF1, SAF2, SAF3, DISTANCE, SAF4
+from parse.parser_constants import CONF_LOG_PREFIX, LOS_LOG_PREFIX, GEO_LOG_PREFIX
+from schemas.tables_attributes import SCENARIO_NAME, SAF1, SAF2, SAF3, DISTANCE, SAF4, SAF6, SAF5, LOS_DURATION_TIME
 from pyspark.sql.functions import lit, col
 import pyspark.sql.functions as F
 
@@ -11,7 +10,6 @@ def compute_SAF1_metric(spark, input_df, output_df):
     (Number of aircraft pairs that will experience a loss of separation within the look-ahead time)
     '''
     df = output_df["OUTPUT"]
-    # Query sentence
     query_rows = input_df[CONF_LOG_PREFIX].groupBy(SCENARIO_NAME).count().select([SCENARIO_NAME, col('count').alias(SAF1)])
     df = df.join(query_rows, on=[SCENARIO_NAME], how='outer')
     return df
@@ -22,7 +20,6 @@ def compute_SAF2_metric(spark, input_df, output_df):
     (Number of aircraft pairs that experience loss of separation)
     '''
     df = output_df["OUTPUT"]
-    # Query sentence
     query_rows = input_df[LOS_LOG_PREFIX].groupBy(SCENARIO_NAME).count().select([SCENARIO_NAME, col('count').alias(SAF2)])
     df = df.join(query_rows, on=[SCENARIO_NAME], how='outer')
     return df
@@ -42,24 +39,26 @@ def compute_SAF4_metric(spark, input_df, output_df):
     (The minimum separation between aircraft during conflicts)
     '''
     df = output_df["OUTPUT"]
-    tmp_df = input_df[LOS_LOG_PREFIX].groupby(SCENARIO_NAME).agg(F.min(DISTANCE).alias("SAF4"))
+    tmp_df = input_df[LOS_LOG_PREFIX].groupby(SCENARIO_NAME).agg(F.min(DISTANCE).alias(SAF4))
     df = df.join(tmp_df, on=[SCENARIO_NAME], how='outer')
     return df
 
-def compute_SAF5_metric(input_df, output_df):
+def compute_SAF5_metric(spark, input_df, output_df):
     '''
     SAF-5: Time spent in LOS
     (Total time spent in a state of intrusion)
     '''
-    # TODO: intrusion_time is "Time_of_min_distance" param of dataframe LOSLOG?
-    result = self.loslog_dataframe.select("ACID1", "ACID2", "Time_of_min_distance").show()
-    return
+    df = output_df["OUTPUT"]
+    query_rows = input_df[LOS_LOG_PREFIX].select(SCENARIO_NAME, col(LOS_DURATION_TIME).alias(SAF5))
+    df = df.join(query_rows, on=[SCENARIO_NAME], how='outer')
+    return df
 
-def compute_SAF6_metric(input_df, output_df):
+def compute_SAF6_metric(spark, input_df, output_df):
     '''
     SAF-6: Geofence violations
     (The number of geofence/building area violations)
     '''
-    #TODO: all the lines of GEOLOG are geofence/building area violations?
-    result = self.geolog_dataframe.select("ACID").count()
-    return result
+    df = output_df["OUTPUT"]
+    query_rows = input_df[GEO_LOG_PREFIX].groupBy(SCENARIO_NAME).count().select([SCENARIO_NAME, col('count').alias(SAF6)])
+    df = df.join(query_rows, on=[SCENARIO_NAME], how='outer')
+    return df
