002da7711d02a9f578e4ab3939095f6dbcf92350
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2022-04-07 16:41:46 +0200

    [develop_everis] Created function to save individual dataframe.

--- platform_code/utils/io_utils.py
+++ platform_code/utils/io_utils.py
@@ -1,11 +1,30 @@
 import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Union, Optional
 
 from loguru import logger
 from pyspark.sql import DataFrame, SparkSession
 
 from utils.config import settings
+
+
+def save_dataframe(file_name: str,
+                   dataframe: DataFrame,
+                   folder: Optional[Union[str, Path]] = None) -> None:
+    """ Saves a dataframes using the name as file name.
+
+    :param file_name: name of the file to save.
+    :param dataframe: dataframes to save.
+    :param folder: folder where the dataframe will be saved inside.
+     where to save the dataframe.
+    """
+    if folder:
+        saving_path = Path(settings.saving_path, folder, f'{file_name.lower()}.parquet')
+    else:
+        saving_path = Path(settings.saving_path, f'{file_name.lower()}.parquet')
+    os.makedirs(saving_path.parent, exist_ok=True)
+    logger.info('Saving dataframe with name: {}.', saving_path.stem)
+    dataframe.write.parquet(str(saving_path))
 
 
 def save_dataframes_dict(dataframes: Dict[str, DataFrame]) -> None:
